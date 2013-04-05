d7041cb74f8fdf71ab3ebe2a73a5055d0e922c5c
damanza2144 <damanza@unm.edu>
2013-04-04 22:59:57 -0600

    mapper modified to trim the ending punctuations

--- assn5/mapper.py
+++ assn5/mapper.py
@@ -1,23 +1,35 @@
 #!/usr/bin/env python
 
 import sys
+import string
 
 # input comes from STDIN (standard input)
 for line in sys.stdin:
 
-	# remove leading and trailing whitespace
-	line = line.strip()
+        # remove leading and trailing whitespace
+        line = line.strip()
 
-	# split the line into words
-	words = line.split()
+        # split the line into words
+        words = line.split()
 
-	# increase counters
-	for word in words:
+        # increase counters
+        for word in words:
 
-		# write the results to STDOUT (standard output);
-		# what we output here will be the input for the
-		# Reduce step, i.e. the input for reducer.py
-		#
-		# tab-delimited; the trivial word count is 1
-		print '%s\t%s' % (word, 1)
+                #word = ''.join(e for e in word if e.isalpha())
+                #word = "".join(e for e in word if e.isalpha())
 
+#               exclude = set(string.punctuation)
+#               word = ''.join(e for e in word if e not in exclude)
+
+#               for c in string.punctuation:
+#                       word = word.replace( c, "" )
+
+                word = word.rstrip( string.punctuation )
+
+                if word:
+                        # write the results to STDOUT (standard output);
+                        # what we output here will be the input for the
+                        # Reduce step, i.e. the input for reducer.py
+                        #
+                        # tab-delimited; the trivial word count is 1
+                        print '%s\t%s' % (word, 1)

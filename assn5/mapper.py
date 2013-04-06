54d870145cd1a06778500445561655d1eaa5a471
damanza2144 <damanza@unm.edu>
2013-04-06 17:45:25 -0600

    added a part3-mapper.py file for part 3 processing in AWS. modified comments and a to trim from the left any punctuation in mapper.py

--- assn5/mapper.py
+++ assn5/mapper.py
@@ -15,18 +15,25 @@ for line in sys.stdin:
         # increase counters
         for word in words:
 
+                # all these cases stripped out the punctuation things 
+                # in between the words
+
                 #word = ''.join(e for e in word if e.isalpha())
                 #word = "".join(e for e in word if e.isalpha())
 
-#               exclude = set(string.punctuation)
-#               word = ''.join(e for e in word if e not in exclude)
+                #exclude = set(string.punctuation)
+                #word = ''.join(e for e in word if e not in exclude)
 
-#               for c in string.punctuation:
-#                       word = word.replace( c, "" )
+                #for c in string.punctuation:
+                #word = word.replace( c, "" )
 
+                # so I decided to use these two functions to strip out 
+                # before and after the word
                 word = word.rstrip( string.punctuation )
+                word = word.lstrip( string.punctuation )
 
                 if word:
+
                         # write the results to STDOUT (standard output);
                         # what we output here will be the input for the
                         # Reduce step, i.e. the input for reducer.py
---
+++ assn5/part3-mapper.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import sys
+
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+
+	# remove leading and trailing whitespace
+	line = line.strip()
+
+	# this is just a dummy mapper where it just outputs what it is given
+	print '%s' % (line)
+

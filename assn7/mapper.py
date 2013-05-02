9951b2c9217c0c02092dc8437e9ec865c721263f
damanza2144 <damanza@unm.edu>
2013-05-01 20:53:08 -0600

    mapper and reducer before matrix multiply and add

--- assn7/mapper.py
+++ assn7/mapper.py
@@ -13,12 +13,12 @@
 #												(number of columns of M, column k), (N, row j, value at row j and column k)
 #
 # reduce algorithm:
-#	For each key (i, k), emit the key-value pair (i, k), pik where pik = sum( mij * njk )
+#	For each key (i, k), emit the key-value pair (i, k), pik where pik = sumj( mij * njk )
 #
 #############################################
 
 import sys
-import string
+#import string
 
 # this will signify if we are processing an M (set to 1) or an N (set to 0) matrix
 is_m = -1
@@ -45,39 +45,36 @@ for line in sys.stdin:
 	# remove leading and trailing whitespace
 	line = line.strip()
 
-	# there must be a line with only "M:" signifying we are starting the M matrix
-	if line == 'M:':
-
-		#print 'start processing m matrix now'
-
-		# set the bit to signify that the next rows and columns are an M matrix
-		is_m = 1
-
-		no_nmatrix_cols = column_counter
-		no_nmatrix_rows = row_counter
-
-		# reset the row counter
-		row_counter = 0
-		continue
-
-	# there must be a line with only "N:" signifying we are starting the N matrix
-	if line == 'N:':
-
-		#print 'start processing n matrix now'
-
-		# set the bit to signify that the next rows and columns are an N matrix
-		is_m = 0
-
-		no_mmatrix_cols = column_counter
-		no_mmatrix_rows = row_counter
-
-		# reset the row counter
-		row_counter = 0
-		continue
-
 	# split the line into columns
 	matrix_columns = line.split()
+
 	if matrix_columns:
+
+		# there must be a line with only "M:" signifying we are starting the M matrix
+		if line == 'M:':
+
+			# set the bit to signify that the next rows and columns are an M matrix
+			is_m = 1
+
+			no_nmatrix_cols = column_counter
+			no_nmatrix_rows = row_counter
+
+			# reset the row counter
+			row_counter = 0
+			continue
+
+		# there must be a line with only "N:" signifying we are starting the N matrix
+		if line == 'N:':
+
+			# set the bit to signify that the next rows and columns are an N matrix
+			is_m = 0
+
+			no_mmatrix_cols = column_counter
+			no_mmatrix_rows = row_counter
+
+			# reset the row counter
+			row_counter = 0
+			continue
 
 		column_counter = 0
 
@@ -107,34 +104,38 @@ if is_m == 0:
 	no_nmatrix_cols = column_counter
 	no_nmatrix_rows = row_counter
 
-#print 'no_mmatrix_cols = %s, no_nmatrix_cols = %s' % (no_mmatrix_cols, no_nmatrix_cols)
-#print 'no_mmatrix_rows = %s, no_nmatrix_rows = %s' % (no_mmatrix_rows, no_nmatrix_rows)
-
 column_counter = 0
 row_counter = 0
+
 for matrix_column in mmatrix_list:
 	
 	#(row i, number of columns of N), (M, column j, value at row i and column j)
 	print '(%s, %s), (M, %s, %s)' % (row_counter, no_nmatrix_cols, column_counter, matrix_column)
 
 	if column_counter == (no_mmatrix_cols - 1):
+
 		column_counter = 0
 		row_counter += 1
+
 	else:
+
 		column_counter += 1
 	
 column_counter = 0
 row_counter = 0
+
 for matrix_column in nmatrix_list:
 	
 	#(number of columns of M, column k), (N, row j, value at row j and column k)
 	print '(%s, %s), (N, %s, %s)' % (no_mmatrix_cols, column_counter, row_counter, matrix_column)
 
 	if column_counter == (no_nmatrix_cols - 1):
+
 		column_counter = 0
 		row_counter += 1
-	else:
-		column_counter += 1
 
+	else:
+
+		column_counter += 1
 
 
--- assn7/reducer.py
+++ assn7/reducer.py
@@ -1,6 +1,51 @@
 #!/usr/bin/env python
 
+#############################################
+# definitions:
+#	M is a matrix with element mij in row i and column j
+#	N is a matrix with element njk in row j and column k
+#	P is a matrix = MN with element pik in row i and column k, where pik = sum( mij * njk )
+#
+# map algorithm:
+#	For each element mij of M, emit a key-value pair (i, k), (M, j, mij) for k=1,2,... number of columns of N.
+#												(row i, number of columns of N), (M, column j, value at row i and column j)
+#	For each element njk of N, emit a key-value pair (i, k), (N, j, njk) for i=1,2,... number of columns of M.
+#												(number of columns of M, column k), (N, row j, value at row j and column k)
+#
+# reduce algorithm:
+#	For each key (i, k), emit the key-value pair (i, k), pik where pik = sumj( mij * njk )
+#
+#############################################
+
 import sys
+import string
+
+def clean_string( current_word ):
+
+	current_word = current_word.strip()
+	current_word = current_word.replace('(','')
+	current_word = current_word.replace(')','')
+
+	return current_word
+
+no_ncols = 0
+no_mcols = 0
+
+no_nrows = 0
+no_mrows = 0
+
+row_counter = 0
+col_counter = 0
+
+matrix_value = 0.0
+
+
+
+mmatrix_list = list()
+nmatrix_list = list()
+pmatrix_list = list()
+
+
 
 # input comes from STDIN
 for line in sys.stdin:
@@ -8,5 +53,40 @@ for line in sys.stdin:
 	# remove leading and trailing whitespace
 	line = line.strip()
 
-	print '%s' % (line)
+	if line:
+
+		# parse the input we got from mapper.py
+		a, b, c, d, e = line.split( ',', 4 )
+
+		a = clean_string(a)
+		b = clean_string(b)
+		c = clean_string(c)
+		d = clean_string(d)
+		e = clean_string(e)
+
+		print '%s' % (line)
+		if c == 'M':
+
+			#(row i, number of columns of N), (M, column j, value at row i and column j)
+			no_ncols = int(b)
+			row_counter = int(a)
+			col_counter = int(d)
+			matrix_value = float(e)
+
+			#print 'processing m matrix here'
+			print 'M: no_ncols=%s, row_counter=%s, col_counter=%s, matrix_value=%s' % (no_ncols, row_counter, col_counter, matrix_value)
+
+		if c == 'N':
+
+			#(number of columns of M, column k), (N, row j, value at row j and column k)
+			no_mcols = int(a)
+			row_counter = int(d)
+			col_counter = int(b)
+			matrix_value = float(e)
+
+			#print 'processing n matrix here'
+			print 'N: no_mcols=%s, row_counter=%s, col_counter=%s, matrix_value=%s' % (no_mcols, row_counter, col_counter, matrix_value)
+
+		#print '%s' % (line)
+		#print 'a=%s, b=%s, c=%s, d=%s, e=%s' % (a, b, c, d, e)
 
